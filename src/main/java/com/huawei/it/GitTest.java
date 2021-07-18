4e19d0eb636274c519a8c04d3f08007218e309db
yangshuai <1945550163@qq.com>
2021-07-18 13:11:03 +0800

    这是第二次提交

---
+++ src/main/java/com/huawei/it/GitTest.java
@@ -0,0 +1,12 @@
+package com.huawei.it;
+
+/**
+ * @Author 帅
+ * @Date 2021/7/18 13:09
+ * @Description TODO
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这只是一个测试");
+    }
+}
